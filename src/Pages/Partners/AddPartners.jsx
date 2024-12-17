5cdb794d57269ddbafe83bd277a0ce6091268223
DabhiDipak1 <parmarjigardhirajlal@gmail.com>
2024-12-17 17:58:43 +0530

    validation

--- src/Pages/Partners/AddPartners.jsx
+++ src/Pages/Partners/AddPartners.jsx
@@ -1,66 +1,148 @@
-import React from 'react'
-import Footer from '../../Components/Footer/Footer'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import Topbar from '../../Components/Topbar/Topbar'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../../Components/Footer/Footer';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import Topbar from '../../Components/Topbar/Topbar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AddPartners() {
+  const [name1, setName1] = useState('');
+  const [percentage1, setPercentage1] = useState('');
+  const [name2, setName2] = useState('');
+  const [percentage2, setPercentage2] = useState('');
+  const [name3, setName3] = useState('');
+  const [percentage3, setPercentage3] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name1 || !percentage1 || !name2 || !percentage2 || !name3 || !percentage3) {
+      toast.error("All Fields Enter!");
+      return;
+    }
+
+     if (!/^[A-Za-z ]+$/.test(name1 && name2 && name3)) {
+          toast.error("Name Can Only  letters And Spaces");
+          return;
+        }
+    
+    if (isNaN(percentage1) || isNaN(percentage2) || isNaN(percentage3)) {
+      toast.error("Percentage Must Be a Valid Number!");
+      return;
+    }
+
+    if (parseFloat(percentage1) < 0 || parseFloat(percentage1) > 100 ||
+        parseFloat(percentage2) < 0 || parseFloat(percentage2) > 100 ||
+        parseFloat(percentage3) < 0 || parseFloat(percentage3) > 100) {
+      toast.error("Percentage Must Be Between 0 And 100!");
+      return ;
+    }
+    else{
+    toast.success(" Successfully");
+    }
+
+
+  };
+
   return (
     <>
       <Sidebar />
-      <div class="content">
+      <div className="content">
         <Topbar />
-        <div class="container-fluid pt-4 px-4">
-          <div class="row g-4">
-            <div class="col-sm-12 col-xl-12">
-              <div class="bg-light rounded h-100 p-4">
+        <div className="container-fluid pt-4 px-4">
+          <div className="row g-4">
+            <div className="col-sm-12 col-xl-12">
+              <div className="bg-light rounded h-100 p-4">
                 <div className="row justify-content-center mx-0">
                   <div className="col-lg-10 col-md-12">
-                    <div class="d-flex justify-content-between mb-3">
-                      <div class="p-2 ">
+                    <div className="d-flex justify-content-between mb-3">
+                      <div className="p-2">
                         <h4>Add Partners</h4>
                       </div>
-                      <div class="p-2 ">
-                        <Link to="/Partners" class="btn">
+                      <div className="p-2">
+                        <Link to="/Partners" className="btn">
                           <i className="bi bi-arrow-left-circle-fill"></i>
                           &nbsp; Back
                         </Link>
                       </div>
                     </div>
-                    <form>
-                      <div class="row">
-                        <div class="col">
-                          <select class="form-select form-select-sm mb-4 w-50" aria-label=".form-select-sm example">
+                    <form onSubmit={handleSubmit}>
+                      <div className="row">
+                        <div className="col">
+                          <select className="form-select form-select-sm mb-4 w-50" aria-label=".form-select-sm example">
                             <option selected>Select Project</option>
                           </select>
                         </div>
                       </div>
-                      <div class="row">
-                        <div class="col">
-                          <input type="text" class="form-control" id="name" placeholder="Name" name="name" />
+                      <div className="row">
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="name1"
+                            placeholder="Name"
+                            value={name1}
+                            onChange={(e) => setName1(e.target.value)}
+                          />
                         </div>
-                        <div class="col">
-                          <input type="text" class="form-control" id="percentage" placeholder="Percentage" name="percentage" />
-                        </div>
-                      </div>
-                      <div class="row pt-4">
-                        <div class="col">
-                          <input type="text" class="form-control" id="name" placeholder="Name" name="name" />
-                        </div>
-                        <div class="col">
-                          <input type="text" class="form-control" id="percentage" placeholder="Percentage" name="percentage" />
-                        </div>
-                      </div>
-                      <div class="row pt-4">
-                        <div class="col">
-                          <input type="text" class="form-control" id="name" placeholder="Name" name="name" />
-                        </div>
-                        <div class="col">
-                          <input type="text" class="form-control" id="percentage" placeholder="Percentage" name="percentage" />
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="percentage1"
+                            placeholder="Percentage"
+                            value={percentage1}
+                            onChange={(e) => setPercentage1(e.target.value)}
+                          />
                         </div>
                       </div>
-                      <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                      <div className="row pt-4">
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="name2"
+                            placeholder="Name"
+                            value={name2}
+                            onChange={(e) => setName2(e.target.value)}
+                          />
+                        </div>
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="percentage2"
+                            placeholder="Percentage"
+                            value={percentage2}
+                            onChange={(e) => setPercentage2(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                      <div className="row pt-4">
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="name3"
+                            placeholder="Name"
+                            value={name3}
+                            onChange={(e) => setName3(e.target.value)}
+                          />
+                        </div>
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="percentage3"
+                            placeholder="Percentage"
+                            value={percentage3}
+                            onChange={(e) => setPercentage3(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                      <button type="submit" className="btn btn-primary mt-3">Submit</button>
                     </form>
+                    <ToastContainer />
                   </div>
                 </div>
               </div>
@@ -68,9 +150,9 @@ function AddPartners() {
           </div>
         </div>
         <Footer />
-      </div >
+      </div>
     </>
-  )
+  );
 }
 
-export default AddPartners
+export default AddPartners;
--- src/Pages/Projects/AddProjects.jsx
+++ src/Pages/Projects/AddProjects.jsx
@@ -1,57 +1,120 @@
-import React from 'react'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import Topbar from '../../Components/Topbar/Topbar'
-import { Link } from 'react-router-dom'
-import Footer from '../../Components/Footer/Footer'
+import React, { useState,useRef } from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Topbar from "../../Components/Topbar/Topbar";
+import { Link } from "react-router-dom";
+import Footer from "../../Components/Footer/Footer";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function AddProjects() {
-    return (
-        <>
-            <Sidebar />
-            <div class="content">
-                <Topbar />
-                <div class="container-fluid pt-4 px-4">
-                    <div class="row g-4">
-                        <div class="col-sm-12 col-xl-12">
-                            <div class="bg-light rounded h-100 p-4">
-                                <div className="row justify-content-center mx-0">
-                                    <div className="col-lg-10 col-md-12">
-                                        <div class="d-flex justify-content-between mb-3">
-                                            <div class="p-2 ">
-                                                <h4>Add Project</h4>
-                                            </div>
-                                            <div class="p-2 ">
-                                                <Link to="/Projects" class="btn">
-                                                    <i className="bi bi-arrow-left-circle-fill"></i>
-                                                    &nbsp; Back
-                                                </Link>
-                                            </div>
-                                        </div>
-                                        <form>
-                                            <div class="mb-3">
-                                                <label for="name" class="form-label">Name : </label>
-                                                <input type="text" class="form-control" id="name" aria-describedby="name" />
-                                                <div id="name" class="form-text">
-                                                </div>
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="file" class="form-label">Upload Unit : </label>
-                                                <input type="file" class="form-control" id="unit" aria-describedby="unit" />
-                                                <div id="unit" class="form-text">
-                                                </div>
-                                            </div>
-                                            <button type="submit" class="btn btn-primary">Submit</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  const [name, setName] = useState("");
+  const [unit, setUnit] = useState(null);
+
+  const files = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name.trim()) {
+      toast.error("Name is required");
+      return;
+    }
+
+    if (!/^[A-Za-z ]+$/.test(name)) {
+      toast.error("Name can only contain letters and spaces");
+      return;
+    }
+    if (name.length < 4) {
+      toast.error("Minimum 4 Characters ");
+      return;
+    }
+
+    if (!unit) {
+      toast.error("Unit is Required");
+      return;
+    }
+
+    if (unit.size > 2 * 1024 * 1024) {
+      toast.error("Image Is Larger Than 3MB");
+      return;
+    }
+
+    if (unit.type == "image/jpeg" || unit.type == "image/png") {
+      toast.success("successfully");
+    } else {
+      toast.error("File Does Not Support. You Must Use .png or .jpg ");
+      return;
+    }
+    console.log(files);
+    setName("");
+    files.current.value=null;
+  };
+
+  return (
+    <>
+      <ToastContainer />
+      <Sidebar />
+      <div className="content">
+        <Topbar />
+        <div className="container-fluid pt-4 px-4">
+          <div className="row g-4">
+            <div className="col-sm-12 col-xl-12">
+              <div className="bg-light rounded h-100 p-4">
+                <div className="row justify-content-center mx-0">
+                  <div className="col-lg-10 col-md-12">
+                    <div className="d-flex justify-content-between mb-3">
+                      <div className="p-2 ">
+                        <h4>Add Project</h4>
+                      </div>
+                      <div className="p-2 ">
+                        <Link to="/Projects" className="btn">
+                          <i className="bi bi-arrow-left-circle-fill"></i>
+                          &nbsp; Back
+                        </Link>
+                      </div>
                     </div>
+                    <form onSubmit={handleSubmit}>
+                      <div className="mb-3">
+                        <label htmlFor="name" className="form-label">
+                          Name :{" "}
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="name"
+                          aria-describedby="name"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="file" className="form-label">
+                          Upload Unit :{" "}
+                        </label>
+                        <input
+                          type="file"
+                          className="form-control"
+                          accept="image/png, image/jpeg"
+                          id="unit"
+                          aria-describedby="unit"
+                          ref={files}
+                          onChange={(e) => setUnit(e.target.files[0])}
+                        />
+                      </div>
+                      <button type="submit" className="btn btn-primary">
+                        Submit
+                      </button>
+                    </form>
+                  </div>
                 </div>
-                <Footer />
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default AddProjects
+export default AddProjects;
