737b0b37b382ae89d84430a476f012ec4819448f
devanshi <devusoni1010@gmail.com>
2025-01-04 15:45:09 +0530

    down payment error solve

--- src/Api/Apikiran.jsx
+++ src/Api/Apikiran.jsx
@@ -4,6 +4,6 @@ export const AddRole = baseUrl + "v1/store-role";
 export const DeleteRole = baseUrl + "v1/roles/delete-role";
 export const RoleHasPermission = baseUrl + "v1/roles-permissions/get-role-permission";
 export const ViewRoleData = baseUrl + "v1/roles/get-role";
-export const  EditRoleData = baseUrl + "v1/roles/get-role";
+export const EditRoleData = baseUrl + "v1/roles/get-role";
 
 
