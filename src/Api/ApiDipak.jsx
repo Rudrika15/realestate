da6c3a015daacf1df742e09debb04c504e6d0acb
DabhiDipak1 <parmarjigardhirajlal@gmail.com>
2025-01-07 17:51:35 +0530

    storeproject api call

--- src/Api/ApiDipak.jsx
+++ src/Api/ApiDipak.jsx
@@ -1,4 +1,4 @@
 export const baseUrl = "https://acsw.in/api/";
 
 export const  demoDownload= baseUrl + "v1/projects/download-excel";
-export const  getProject = baseUrl + "v1/projects/get-project";
+export const  storeProject = baseUrl + "v1/projects/store-project";
