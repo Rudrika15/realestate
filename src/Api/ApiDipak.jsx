a5ab51e681b3ef690b5360dc87d70a2235a0f6ed
ravirajsinh <ravirajsinh.m.gohil@gmail.com>
2025-01-27 17:26:23 +0530

    code merge

--- src/Api/ApiDipak.jsx
+++ src/Api/ApiDipak.jsx
@@ -1,19 +1,20 @@
 export const baseUrl = "https://acsw.in/api/";
-  // project //
-export const  demoDownload= baseUrl + "v1/projects/download-excel";
-export const  storeProject = baseUrl + "v1/projects/store-project";
-export const  getProject = baseUrl + "v1/projects/get-project";
-export const  projectWiseUnit = baseUrl + "v1/projects/get-project-unit";
 
-  // partner //
-export const addPartner = baseUrl + "v1/partner/add-partner";          
-export const getPartner = baseUrl + "v1/partner/get-partner";          
-export const editPartner = baseUrl + "v1/partner/get-partner-by-id";  
+// project //
+export const demoDownload = baseUrl + "v1/projects/download-excel";
+export const storeProject = baseUrl + "v1/projects/store-project";
+export const getProject = baseUrl + "v1/projects/get-project";
+export const projectWiseUnit = baseUrl + "v1/projects/get-project-unit";
+
+// partner //
+export const addPartner = baseUrl + "v1/partner/add-partner";
+export const getPartner = baseUrl + "v1/partner/get-partner";
+export const editPartner = baseUrl + "v1/partner/get-partner-by-id";
 export const fetchPartner = baseUrl + "v1/partner/get-single-partner"
-export const deletePartner = baseUrl + "v1/partner/delete-partner";    
-export const updatePartner = baseUrl + "v1/partner/update-partner";    
-export const partnerPartnerDelete = baseUrl +"v1/partner/partner-delete"
+export const deletePartner = baseUrl + "v1/partner/delete-partner";
+export const updatePartner = baseUrl + "v1/partner/update-partner";
+export const partnerPartnerDelete = baseUrl + "v1/partner/partner-delete"
 
 // roles-wise-permissions //
 
-export const rolesWisePermissions = baseUrl +"v1/users/roles-wise-permissions"
+export const rolesWisePermissions = baseUrl + "v1/users/roles-wise-permissions"
--- src/Api/Apikiran.jsx
+++ src/Api/Apikiran.jsx
@@ -1,14 +1,13 @@
 export const baseUrl = "https://acsw.in/api/";
 
 
-
 export const AddRoles = baseUrl + "v1/roles/store-role";
 export const DeleteRole = baseUrl + "v1/roles/delete-role";
 export const RoleHasPermission = baseUrl + "v1/roles-permissions/get-role-permission/";
 export const PermissionFetch = baseUrl + "v1/permissions/get-permission";
 export const ViewRoleData = baseUrl + "v1/roles/get-role";
-export const  EditRoleData = baseUrl + "v1/roles/get-role/";
-export const updateRole =  baseUrl + "v1/roles/update-role";
+export const EditRoleData = baseUrl + "v1/roles/get-role/";
+export const updateRole = baseUrl + "v1/roles/update-role";
 export const AddPermissions = baseUrl + "v1/permissions/store-permission";
 export const RoleAndPermission = baseUrl + "v1/roles-permissions/store-role-permission";
 export const updatedPermission = baseUrl + "v1/permissions/update-permission";
